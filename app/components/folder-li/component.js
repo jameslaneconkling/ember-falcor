a3a3ff40afc63bb91963887e4285501af43c9972
James Conkling <james.lane.conkling@gmail.com>
2016-07-16 23:23:15 -0400

    add index to folder-lis

--- app/components/folder-li/component.js
+++ app/components/folder-li/component.js
@@ -4,6 +4,7 @@ import falcor,
        {url2Path}         from '../../models/falcor';
 
 const FolderItem = FalcorComponent.extend({
+  tagName: 'li',
   actions: {
     deleteFolder() {
       let callPath = ['foldersById', this.get('folder.id'), 'delete'];
--- app/components/folder-list/component.js
+++ app/components/folder-list/component.js
@@ -2,12 +2,15 @@ import Ember              from 'ember';
 import FalcorComponent    from '../falcor-component/component';
 import FolderItem         from '../folder-li/component';
 
-const FolderList = FalcorComponent.extend({});
+const FolderList = FalcorComponent.extend({
+  tagName: 'ul'
+});
 
 FolderList.reopenClass({
   queries: {
     folders: (params) => ['folderList', {from: 1, to: params.folderCount}, ...FolderItem.getQuery('folder')]
   },
+
   queryParams: {
     folderCount: 5
   }
